4bde44546fc1b1b4299171aa10b97f43cc2c88c2
Felix Nderitu <newtnfelix@gmail.com>
2021-10-20 18:13:16 +0300

    add divs for styling in profile

--- Documents/Moringa-core-projects/github-api/src/app/app.module.ts
+++ Documents/Moringa-core-projects/github-api/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileService } from './profile.service';
 import { ProfileComponent } from './profile/profile.component';
+import { FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule, 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ProfileService],
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
